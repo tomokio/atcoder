b10296c129e618d67838c38aac37a150ade2744a
TOMOKI O <64746406+tomokio@users.noreply.github.com>
2023-05-05 14:06:11 +0900

    Added the new algorithm.

--- python/algorithm/utils.py
+++ python/algorithm/utils.py
@@ -1,3 +1,4 @@
+# nの約数を列挙
 def list_divisor(n: int) -> list:
     lower_divisors , upper_divisors = [], []
     i = 1
@@ -8,3 +9,17 @@ def list_divisor(n: int) -> list:
                 upper_divisors.append(n//i)
         i += 1
     return lower_divisors + upper_divisors[::-1]
+
+# エラトステネスの篩：x以下の素数を列挙
+def eratosthenes(x: int) -> list:
+    nums = [i for i in range(x + 1)]
+    root = int(pow(x, 0.5))
+
+    for i in range(2, root + 1):
+        if nums[i] != 0:
+            for j in range(i, x + 1):
+                if i * j >= x + 1:
+                    break
+                nums[i * j] = 0
+
+    return sorted(list(set(nums)))[2:] # setで複数個ある0を1つに [2:]で0と1を除く素数を抽出
